ab8083420f2a72d57280e0458209c1b7c2a935fa
Rafael Neto <neto.webprojetos@gmail.com>
2023-06-08 12:49:09 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
 const DB_HOST = 'localhost';
 const DB_NAME = 'livro_ocorrencia';
-const DB_USER = 'alerta';
-const DB_PASS = 'Galerta#2021';
+const DB_USER = '';
+const DB_PASS = '#';
 try {
   $conn = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
